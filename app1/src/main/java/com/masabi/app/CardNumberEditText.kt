9b11dbd5af8ffa642beb61a7e1d1228c123921ce
Adil Hussain <>
2023-05-10 16:41:37 +0100

    (app1) Changed `CardNumberEditText` to extend `FrameLayout` and contain an ` EditText` rather than extending `EditText`.

--- app1/src/main/java/com/masabi/app/CardNumberEditText.kt
+++ app1/src/main/java/com/masabi/app/CardNumberEditText.kt
@@ -1,32 +1,57 @@
 package com.masabi.app
 
 import android.content.Context
+import android.text.Editable
+import android.text.InputType
 import android.util.AttributeSet
-import androidx.appcompat.widget.AppCompatEditText
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.FrameLayout
 import androidx.core.widget.addTextChangedListener
 
 class CardNumberEditText @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet?,
-    defStyleAttr: Int = androidx.appcompat.R.attr.editTextStyle
-) : AppCompatEditText(context, attrs, defStyleAttr) {
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
 
-    init {
-        addTextChangedListener {
-            if (it == null) {
-                return@addTextChangedListener
-            }
-
-            val text = it.toString()
-            val formattedText = text.replace(" ", "").chunked(4).joinToString(" ")
-
-            if (text != formattedText) {
-                setText(formattedText)
-                setSelection(length())
-            }
-        }
-    }
+    private val editText = EditText(context, attrs)
 
     val cardNumber: String
-        get() = text.toString().replace(" ", "")
+        get() = editText.text.toString().replace(" ", "")
+
+    init {
+        layoutParams = LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+
+        editText.layoutParams = LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+
+        editText.inputType = InputType.TYPE_CLASS_NUMBER
+        editText.minLines = 1
+        editText.maxLines = 1
+
+        editText.addTextChangedListener { afterTextChanged(it) }
+
+        addView(editText)
+    }
+
+    private fun afterTextChanged(editable: Editable?) {
+        if (editable == null) {
+            return
+        }
+
+        val text = editable.toString()
+
+        val formattedText = text.replace(" ", "").chunked(4).joinToString(" ")
+
+        if (text != formattedText) {
+            editText.setText(formattedText)
+            editText.setSelection(editText.length())
+        }
+    }
 }
