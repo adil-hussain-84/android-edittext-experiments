688498e8d5d35a727e56c8b9f850277f074a467c
Adil Hussain <>
2023-06-08 18:11:21 +0100

    (app1) Added a documentation comment in `CursorPositionCalculator`.

--- app1/src/main/java/com/masabi/app/CursorPositionCalculator.kt
+++ app1/src/main/java/com/masabi/app/CursorPositionCalculator.kt
@@ -2,6 +2,16 @@ package com.masabi.app
 
 object CursorPositionCalculator {
 
+    /**
+     * Calculates where the cursor should be placed within `formattedText` taking into account `originalTextUpToOldCursorPosition`.
+     *
+     * This function assumes that:
+     * 1. The non-space characters in `originalTextUpToOldCursorPosition` are a subset of the non-space characters in `formattedText`.
+     * 2. The non-space characters in `originalTextUpToOldCursorPosition` are in the same exact order as the non-space characters in `formattedText`.
+     *
+     * @param formattedText The formatted text which may have spaces in different places than the original (i.e. pre-formatted) text.
+     * @param originalTextUpToOldCursorPosition The original (i.e. pre-formatted) text up to where the cursor was prior to formatting.
+     */
     fun calculateCursorPositionInFormattedText(formattedText: String, originalTextUpToOldCursorPosition: String): Int {
         var indexInFormattedText = 0
         var indexInOriginalText = 0
